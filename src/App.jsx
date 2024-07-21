835903dae2076698d5802b1b58bcc43018fc828c
samet-byte <sametbayat@hacettepe.edu.tr>
2024-07-21 23:42:30 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MovieFrame from "./components/MovieFrame.jsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <MovieFrame />
+      </div>
+    </>
+  )
+}
+
+export default App
